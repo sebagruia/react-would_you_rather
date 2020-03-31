968bc350f5f7923c1f515f621031f03387c89566
sebagruia <sebastiangruia@gmail.com>
2020-03-31 19:23:47 +0200

    Leaderbord component displays user stats in descending order

--- src/component/Leaderboard.js
+++ src/component/Leaderboard.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 import UserStats from "../component/UserStats";
 
-const Leaderbord =(props)=>{
-    const{users} = props
-    return(
-      <UserStats />
-    );
-}
+const Leaderbord = props => {
+  const { users } = props;
+  // The user array is sorted according to the SUM of "answered questions+created questions" in the descending order
+  const sortedUSers = users.sort((a,b)=>{
+    return (Object.keys(b.answers).length + b.questions.length) -  (Object.keys(a.answers).length + a.questions.length); 
+  });
+  console.log(sortedUSers)
+  return (
+    <div className="leaderboard">
+      {sortedUSers.map(user => {
+        return <UserStats key={user.id} user={user} />
+      })}
+    </div>
+  );
+};
 
 export default Leaderbord;
--- src/component/UserStats.js
+++ src/component/UserStats.js
@@ -1,20 +1,24 @@
 import React from "react";
 
-const UserStats = () => {
+const UserStats = ({user}) => {
+    const numberOfQuestionsAnswered = Object.keys(user.answers).length;
+    const numberOfQuestionsCreated = Object.keys(user.questions).length;
+    const score = numberOfQuestionsAnswered + numberOfQuestionsCreated;
+    
   return (
     <div className="userStats-container">
       <div className="userStats-avatar">
-        <img src="../../avatars/sarahedo.png" alt="avatar" />
+        <img src={user.avatarURL} alt="avatar" />
       </div>
       <div className="userStats-statistic">
         <div className="userStats-statistic-text">
-          <h5>John Doe</h5>
+          <h5>{user.name}</h5>
           <div className="answered-questions">
             <div>
               <h6>Answered questions</h6>
             </div>
             <div>
-              <h6>3</h6>
+              <h6>{numberOfQuestionsAnswered}</h6>
             </div>
           </div>
           <div className="created-questions">
@@ -22,7 +26,7 @@ const UserStats = () => {
               <h6>Created questions</h6>
             </div>
             <div>
-              <h6>6</h6>
+              <h6>{numberOfQuestionsCreated}</h6>
             </div>
           </div>
         </div>
@@ -32,7 +36,7 @@ const UserStats = () => {
           <h6>Score</h6>
         </div>
         <div className="userStats-score-number">
-          <p>20</p>
+          <p>{score}</p>
         </div>
       </div>
     </div>
