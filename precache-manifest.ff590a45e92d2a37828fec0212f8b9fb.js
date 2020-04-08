a1199f432428b03cdc169946bfecac4bf04add27
sebagruia <sebastiangruia@gmail.com>
2020-04-08 22:44:33 +0200

    Updates

---
+++ precache-manifest.ff590a45e92d2a37828fec0212f8b9fb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3693c1a1f17eb2e25724a7ab639f843",
+    "url": "/react-would_you_rather/index.html"
+  },
+  {
+    "revision": "e317baea802bb07eb6dc",
+    "url": "/react-would_you_rather/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "726f3e09c5a65ee9cd7d",
+    "url": "/react-would_you_rather/static/css/main.31d74505.chunk.css"
+  },
+  {
+    "revision": "e317baea802bb07eb6dc",
+    "url": "/react-would_you_rather/static/js/2.d1a9175a.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-would_you_rather/static/js/2.d1a9175a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726f3e09c5a65ee9cd7d",
+    "url": "/react-would_you_rather/static/js/main.f13ea44d.chunk.js"
+  },
+  {
+    "revision": "edabadb4f6915305073b",
+    "url": "/react-would_you_rather/static/js/runtime-main.32529cff.js"
+  },
+  {
+    "revision": "50d048d725b335642fdd813466f3904f",
+    "url": "/react-would_you_rather/static/media/johndoe.50d048d7.png"
+  },
+  {
+    "revision": "2056c12cdfbd3f2959ff27a8925ef08f",
+    "url": "/react-would_you_rather/static/media/logo.2056c12c.png"
+  },
+  {
+    "revision": "b5af26a6b5e400bcd6b4574db626e201",
+    "url": "/react-would_you_rather/static/media/sarahedo.b5af26a6.png"
+  },
+  {
+    "revision": "8366a16ce32a5dbf70236e48487e5644",
+    "url": "/react-would_you_rather/static/media/tylermcginnis.8366a16c.png"
+  }
+]);
